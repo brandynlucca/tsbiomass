5de65ef847d588a9ae3c87157f7d74fd747cf8db
brandynlucca <brandyn.lucca@gmail.com>
2026-06-26 17:31:52 -0700

    Drop superfluous FAO areas mapping data object

--- R/data.R
+++ R/data.R
@@ -1,11 +1,3 @@
-#' FAO area polygons
-#'
-#' A tibble containing FAO major area metadata and polygons.
-#'
-#' @format A tibble with FAO area metadata and polygon geometry columns.
-#' @usage data(fao_areas)
-"fao_areas"
-
 #' Preprocessed candidate models table
 #'
 #' A tibble containing species- and study-level metadata traits used for
